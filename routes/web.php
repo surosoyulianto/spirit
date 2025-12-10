24b5064aabdc10de5746e9e2a7265d151a609172
surosoyulianto <suroso.yulianto@gmail.com>
2025-12-10 08:54:23 +0700

    Enchane: web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,27 @@
-Route::resource('products', ProductController::class);
-    Route::resource('customers', CustomerController::class);
-    Route::resource('manufacturing', ManufacturingController::class);
-=======
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ManufacturingController;
+use App\Http\Controllers\InvoiceController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::resource('invoices', InvoiceController::class);
     Route::resource('products', ProductController::class);
     Route::resource('customers', CustomerController::class);
     Route::resource('manufacturing', ManufacturingController::class);
     Route::resource('inventories', \App\Http\Controllers\InventoryController::class);
+});
